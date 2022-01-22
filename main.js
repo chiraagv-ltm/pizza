98556812182f31971c1a0ca651a222f7d4bec41f
chiraagv-ltm <82768667+chiraagv-ltm@users.noreply.github.com>
2022-01-21 20:25:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var htmldata;
 htmldata="<ol class='menulist'>"
 menu_list_array.sort();
 for(var i=0;i<menu_list_array.length;i++){
-    htmldata=htmldata+'<li>' + menu_list_array[i] + '<li>'
+    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
 }
 htmldata=htmldata + "</ol>"
 document.getElementById("display_menu").innerHTML=htmldata;
